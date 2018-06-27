44ce32562094262e1a99ee531db16367a8eb682b
邓强锡 <dengqiangxi@askingdata.com>
2018-06-27 16:16:19 +0800

    bugfix

--- jishux/misc/clean_tools.py
+++ jishux/misc/clean_tools.py
@@ -5,9 +5,10 @@ import re
 from urllib.parse import urlsplit
 from scrapy import Selector
 from bs4 import BeautifulSoup
+
 # 需要替换的img src属性
 img_lazy_src = [
-    'src', 'data-original', 'data-original-src='
+    'src', 'data-original', 'data-original-src'
 ]
 
 
@@ -43,7 +44,7 @@ def clean_tags(item):
     if (data_original and src) or (data_src and src):
         content_html = content_html.replace(' src=', ' src2=')
     for lazy_src in img_lazy_src:
-        content_html = content_html.replace(' ' + lazy_src, ' data-src=')
+        content_html = content_html.replace(' ' + lazy_src + '=', ' data-src=')
     # 清除HTML多余的style scripts comments
     soup = BeautifulSoup(content_html, 'lxml')
     content_html = _remove_all_attrs_except_saving(soup)
